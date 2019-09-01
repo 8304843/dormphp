cce3c36b2cae4c44b394bc9f7f9d40f9c414ed5b
8304843 <317406241@qq.com>
2019-09-01 13:14:35 +0800

    2019/09/01 13点13分
    第一次上传

---
+++ conn.php
@@ -0,0 +1,14 @@
+<?php	
+    header("Access-Control-Allow-Origin:*");
+//连接数据库
+	$servername = "127.0.0.1:3306";
+	$username = "root";
+	$password = "123456";
+	$dbname = "jmmes";	
+	$conn = new mysqli($servername, $username, $password, $dbname);	
+	if ($conn->connect_error) {
+		die("Connection failed: " . $conn->connect_error);
+	}else{
+//		echo "Connected successfully"; 
+	}	
+?> 
